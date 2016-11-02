88ccb6a9c240a7aa8c5ff7c2e2963bd6cceb7b3a
Charles Olupot <olupotcharles@gmail.com>
2016-11-02 18:57:20 +0300

    Added argument names and some comments in the importation of EID

--- import_eid.py
+++ import_eid.py
@@ -2,8 +2,8 @@ from data_import import read, register, construct_obs, transform_list, group_dat
 from mappings.eid_summary_map import eid_summary_map
 from mappings.eid_encounter_map import eid_encounter_map
 
-eid_registration = read('./data/baylor/eid/eid_summary.csv')
-eid_encounters = read('./data/baylor/eid/eid_encounters.csv')
+eid_registration = read('./test_data/eid/eid_summary.csv')
+eid_encounters = read('./test_data/eid/eid_encounters.csv')
 
 eid_summary_variables = read('./csv_forms/eid_summary.csv')
 eid_encounter_variables = read('./csv_forms/eid_encounter.csv')
@@ -11,6 +11,12 @@ eid_encounter_variables = read('./csv_forms/eid_encounter.csv')
 summary_variables = transform_list(eid_summary_variables)
 encounter_variables = transform_list(eid_encounter_variables)
 
-register(eid_registration[1:], 4, 6, 7, [None, 1, None, None, None, None], {'2': '6'})
-construct_obs(eid_registration[1:], 2, 12, 3, 16, eid_summary_map, summary_variables)
-construct_obs(eid_encounters[1:], 1, 11, 2, 12, eid_encounter_map, encounter_variables)
+# Register patients
+register(eid_registration[1:], names_column=4, gender_column=6, birth_date_column=7,
+         address_columns=[None, 1, None, None, None, None], identifier_columns={'2': '6'})
+# Summary page and observations
+construct_obs(eid_registration[1:], identifier_column=2, form_id=12, visit_column=3, encounter_type=16,
+              mapped_dict=eid_summary_map, concept_answer_map=summary_variables, location_id=2)
+# Encounter page and observations
+construct_obs(eid_encounters[1:], identifier_column=1, form_id=11, visit_column=2, encounter_type=12,
+              mapped_dict=eid_encounter_map, concept_answer_map=encounter_variables, location_id=2)
